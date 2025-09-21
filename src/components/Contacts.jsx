ce53db0575d6d9cfc2e8a1c130ce07ccdb0fc93a
SheilaSusan <susansheila08@gmail.com>
2025-09-21 12:52:50 +0300

    Add header section and contacts

---
+++ src/components/Contacts.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Contacts(){
+    return(
+        <div className="contacts">
+            <h1>Sheila Susan</h1>
+            <p className="title">Frontend Developer</p>
+        </div>
+    )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import sue from "../assets/sue.jpg"
+
+export default function Header(){
+    return(
+        <header>
+            <img src={sue} alt="Sue profile pic" />
+        </header>
+    )
+}
